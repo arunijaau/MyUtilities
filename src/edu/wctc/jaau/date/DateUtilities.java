22bc705a9f7e14c657a462b49a96cd94fe2acca4
Aruni <Aruni@maxxi2.attlocal.net>
2017-04-19 13:10:25 -0500

    Second commit - Added more methods and documentation.

--- src/edu/wctc/jaau/date/DateUtilities.java
+++ src/edu/wctc/jaau/date/DateUtilities.java
@@ -1,16 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.wctc.jaau.date;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * Singleton utility class to simplify managing and using dates and times according to new 
  * Java Date/Time API(Java JDK 8)
+ * <p>
+ * Change history:
+ * <ul>
+ * <li>04-17-2017 - Initial version.</li>
+ * <li>04-19-2017 - Added more methods and documentation.</li>
+ * </ul>
+ * </p>
  * @author Aruni Jayasooriya
  * @version 1.00
  * @since 1.8
@@ -55,7 +59,7 @@ public class DateUtilities {
     /**
      * Format a <code>Date</code> according to the default pattern.
      * @param date - a <code>Date</code> object.
-     * @return a date formatted according to the default format pattern. 
+     * @return a date formatted according to the default format pattern (MMM d yyyy hh:mm). 
      * @throws IllegalArgumentException if date or format pattern is null.
      */
     public String toString(LocalDateTime date) throws IllegalArgumentException {
@@ -66,23 +70,26 @@ public class DateUtilities {
     /**
      * Format a <code>Date</code> according to a given pattern.
      * @param date - a <code>Date</code> object.
-     * @param pattern - an enum representing a <code>FormatPattern</code> .
+     * @param pattern - an enum representing a <code>FormatPattern</code>.
      * @return a date formatted according to the specified pattern.
      * @throws IllegalArgumentException if date or format pattern is null.
      */
-    public String toString(LocalDateTime date, FormatPattern pattern) throws IllegalArgumentException {     
+    public String toString(LocalDateTime date, FormatPattern pattern) throws IllegalArgumentException {    
+        if(pattern == null){
+            throw new IllegalArgumentException("Pattern provided should not be null.");
+        }
         return this.toString(date, pattern.getPattern());
         
     }
 
     /**
-     * Format a <code>Date</code> according to a given pattern.
+     * Format a <code>Date</code> according to a given customized pattern.
      * @param date - a <code>Date</code> object.
      * @param pattern - a <code>String</code> of required pattern.
-     * @return a date formatted according to the pattern given.
+     * @return a date formatted according to the customized pattern given.
      * @throws IllegalArgumentException if date or format pattern is null or format pattern is empty.
      */
-    public String toString(LocalDateTime date, String pattern)  throws IllegalArgumentException {
+    public String toString(LocalDateTime date, String pattern) throws IllegalArgumentException {
         if (date == null || pattern == null || pattern.isEmpty()) {
             throw new IllegalArgumentException("Date or pattern provided should not be null.");
         }
@@ -91,5 +98,35 @@ public class DateUtilities {
         return formattedString;
     }
     
+    /**
+     * Given date string is parsed to the default format pattern (MMM d yyyy hh:mm). 
+     * @param dateTimeString - a <code>String</code> representation of a date and time.
+     * @return a DateTime object parsed according to the default format pattern (MMM d yyyy hh:mm). 
+     * @throws IllegalArgumentException if dateTime string is null or empty.
+     * @throws DateTimeParseException if dateTime string cannot be parsed as a DateTime object.
+     */
+    public LocalDateTime getDateTimeObject(String dateTimeString) throws IllegalArgumentException, DateTimeParseException {
+        return this.getDateTimeObject(dateTimeString, FormatPattern.DEFAULT);
+    }
+    
+    public LocalDateTime getDateTimeObject(String dateTimeString, FormatPattern pattern) throws IllegalArgumentException, DateTimeParseException {
+        if(pattern == null){
+            throw new IllegalArgumentException("Pattern provided should not be null.");
+        }
+        return this.getDateTimeObject(dateTimeString, pattern.getPattern());
+    }
+    
+    public LocalDateTime getDateTimeObject(String dateTimeString, String pattern) throws IllegalArgumentException, DateTimeParseException {
+        if(dateTimeString == null || dateTimeString.isEmpty() || pattern == null || pattern.isEmpty()){
+            throw new IllegalArgumentException("DateTime string or pattern provided should not be null.");
+        }
+        try{
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+            LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
+            return dateTime;
+        }catch(DateTimeParseException dtp){
+            throw dtp;
+        }
+    }
 }
 
